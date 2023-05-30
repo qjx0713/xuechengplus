4399719f2445dca694527e681e193efe48bdd55f
qianjinxing <qianjx1@chinatelecom.cn>
2023-05-30 14:57:56 +0800

    day8 course publish

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -4,4 +4,8 @@ import com.xuecheng.content.model.dto.CoursePreviewDto;
 
 public interface CoursePublishService {
     public CoursePreviewDto getCoursePreviewInfo(Long courseId);
+
+    public void commitAudit(Long companyId,Long courseId);
+
+    public void publish(Long companyId,Long courseId);
 }
